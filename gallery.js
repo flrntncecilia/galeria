4941594f2537943edd88baf77930fb06b2f820c9
cecília <ceciliaflorentin12@gmail.com>
2023-02-16 14:37:57 -0300

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -2,27 +2,26 @@ let thumbnails = document.getElementsByClassName('thumbnail')
         
 let activeImages = document.getElementsByClassName('active')
 
-for (var i=0; < thumbnails.length; i++){
+for (var i=0; i < thumbnails.length; i++){
     
-    thumbnails[i].addEventListener('mousever', function(){
+    thumbnails[i].addEventListener('mouseover', function(){
         console.log(activeImages)
 
         if (activeImages.length > 0){
             activeImages[0].classList.remove('active')
-            }
+        }
                 
             this.classList.add('active')
             document.getElementById('featured').src = this.src
-            })
-        }
+    })
+}
+let buttonRight = document.getElementById('slideRight');
+		let buttonLeft = document.getElementById('slideLeft');
 
-        let buttonRight = document.getElementById('slideRight');
-        let buttonLeft = document.getElementById('slideLeft');
+		buttonLeft.addEventListener('click', function(){
+			document.getElementById('slider').scrollLeft -= 180
+		})
 
-        buttonLeft.addEventListener('click', function(){
-            document.getElementById('slider').scrollLeft -= 180
-        })
-
-        buttonRight.addEventListener('click', function(){
-            document.getElementById('slider').scrollRight += 180
-        })
+		buttonRight.addEventListener('click', function(){
+			document.getElementById('slider').scrollLeft += 180
+		})
